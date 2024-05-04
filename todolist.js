8d1d698b995eed4c946cb54b80b623bade4e417d
Sidra Muneer <129052326+SidraMuneer123@users.noreply.github.com>
2024-05-04 10:02:37 +0400

    Add files via upload

---
+++ todolist.js
@@ -0,0 +1,34 @@
+Todolist = [
+  {
+    id : 1,
+    'title' : 'Buy Groceries',
+    "Description" : "I will go to supermarket in order to buy some fruits , Vegetables , Grains and some dairy products",
+    "IsCompleted" : false
+},
+  {
+    id : 2,
+    'title' : 'Clean the House',
+    "Description" : "I have to clean the house like moping and dusting",
+    "IsCompleted" : false
+},
+  {
+    id : 3,
+    'title' : 'Go to Libraray',
+    "Description" : "I have go to library in order to return the book",
+    "IsCompleted" : true
+},
+  {
+    id : 4,
+    'title' : 'Make a Milkshake',
+    "Description" : "I have to make a banana milkshake ",
+    "IsCompleted" : false
+},
+  {
+    id : 5,
+    'title' : 'Complete my homework',
+    "Description" : "I have to complete my homework... ",
+    "IsCompleted" : false
+},
+]
+
+module.exports = Todolist;
